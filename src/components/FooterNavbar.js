e745d04636816c8aca870e2f7c384451d6e585a1
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-26 18:09:58 +0200

    created navbar and functions for navigation

--- src/components/FooterNavbar.js
+++ src/components/FooterNavbar.js
@@ -1,61 +1,48 @@
 import React, { useState } from "react";
 import frame from "../resource/icons/frame.png";
 import home from "../resource/icons/home.png";
-import Profile from "../resource/icons/Profile.png";
+import profile from "../resource/icons/profile.png";
 import search from "../resource/icons/search.png";
 import redFrame from "../resource/icons/redFrame.png";
 import redHome from "../resource/icons/redHome.png";
 import redProfile from "../resource/icons/redProfile.png";
 import redSearch from "../resource/icons/redSearch.png";
+import { Link } from "react-router-dom";
 
 function FooterNavbar() {
-  const [clickHome, setClickHome] = useState(true);
-  const [clickSearch, setClickSearch] = useState(true);
-  const [clickFrame, setClickFrame] = useState(true);
-  const [clickProfile, setClickProfile] = useState(true);
+  const [activeNav, setActiveNav] = useState(null);
+
+  const handleNavClick = (navId) => {
+    setActiveNav(navId);
+  };
+
+  const navbar = [
+    { id: 1, color1: home, color2: redHome, path: "/" },
+    { id: 2, color1: search, color2: redSearch, path: "/search" },
+    { id: 3, color1: frame, color2: redFrame, path: "/upload" },
+    { id: 4, color1: profile, color2: redProfile, path: "/profile" },
+  ];
+
+  const getNavIcon = (nav) => {
+    if (activeNav === nav.id) {
+      return <img src={nav.color2} alt="homeimg" className="nav-icon" />;
+    } else {
+      return <img src={nav.color1} alt="homeimg" className="nav-icon" />;
+    }
+  };
+
   return (
     <div>
-      <footer className="footer">
-        <button
-          className="home-footer"
-          onClick={() => setClickHome(!clickHome)}
-        >
-          {clickHome ? (
-            <img src={home} alt="homeimg" className="home-icon" />
-          ) : (
-            <img src={redHome} alt="homeimg" className="home-icon" />
-          )}
-        </button>
-        <button
-          className="search-footer"
-          onClick={() => setClickSearch(!clickSearch)}
-        >
-          {clickSearch ? (
-            <img src={search} alt="search" className="search-icon" />
-          ) : (
-            <img src={redSearch} alt="search" className="search-icon1" />
-          )}
-        </button>
-        <button
-          className="frame-footer"
-          onClick={() => setClickFrame(!clickFrame)}
-        >
-          {clickFrame ? (
-            <img src={frame} alt="frame" className="frame-icon" />
-          ) : (
-            <img src={redFrame} alt="frame" className="frame-icon" />
-          )}
-        </button>
-        <button
-          className="profile-footer"
-          onClick={() => setClickProfile(!clickProfile)}
-        >
-          {clickProfile ? (
-            <img src={Profile} alt="profile" className="profile-icon" />
-          ) : (
-            <img src={redProfile} alt="profile" className="profile-icon" />
-          )}
-        </button>
+      <footer className="footer-navbar">
+        {navbar.map((nav) => (
+          <button
+            key={nav.id}
+            className="home-footer"
+            onClick={() => handleNavClick(nav.id)}
+          >
+            <Link to={nav.path}>{getNavIcon(nav)}</Link>
+          </button>
+        ))}
       </footer>
     </div>
   );
