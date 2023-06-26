3386eaa1942026ca42449c51d02ed3e21f297c45
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-26 12:58:23 +0200

    deleted tailwind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "../src/index.css";
+import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
