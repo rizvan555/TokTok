8de5e78bea6bf69b97088a35f1e2939147a4822f
Jenseko <jenscode23@gmail.com>
2023-06-26 15:49:04 +0200

    add ProfileMainBar comp

---
+++ src/components/ProfileFacts.js
@@ -0,0 +1,24 @@
+import profile_image from '../resource/images/Ellipseprofile_image_small.png';
+import profile_edit_icon from '../resource/icons/Edit Squareprofile_image_edit_icon.png';
+
+
+// --- style --------------------------
+import '../css/own_profile.css';
+// ------------------------------------
+
+const ProfileFacts = () => {
+    return (
+        <div>
+            <article>
+                <img src={profile_image} alt="profile_image_user" />
+                <img src={profile_edit_icon} alt="edit_icon" />
+                <h1>John Doe</h1>
+                <h3>UI/UX Designer</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus sunt ipsa minima ipsum dolores animi delectus magni odit reiciendis voluptatum?</p>
+
+            </article>
+        </div>
+    );
+}
+
+export default ProfileFacts;
---
+++ src/pages/own_profile.js
@@ -0,0 +1,18 @@
+// --- style --------------------------
+import '../css/own_profile.css';
+// ------------------------------------
+
+// Import - components ----------------
+import { ProfileMainBar } from '../components/ProfileMainBar';
+// ------------------------------------
+
+
+function own_Profile() {
+    return (
+        <div>
+            <ProfileMainBar />
+        </div>
+    )
+}
+
+export default own_Profile;
