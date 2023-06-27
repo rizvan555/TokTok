12df8e11cd25d363b357865d1475de5e28e8bbae
codesfromannywhere <codesfromannywhere@gmail.com>
2023-06-27 16:09:49 +0200

    changed files

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from '@clerk/nextjs'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ClerkProvider>
         <App />
-      </ClerkProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
