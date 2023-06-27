ed11a6def87979f009afa9e7fd9eea5ead6e178f
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-27 22:05:28 +0200

    created search page and search function with filter methode

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
 import CommentsPage from "./pages/commentsPage";
-import UploadPage from "./pages/upload"
-import NewPost from "./pages/post"
+import UploadPage from "./pages/upload";
+import NewPost from "./pages/post";
+import SearchAll from "./pages/searchAll";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/commentsPage" element={<CommentsPage />} />
+        <Route path="/search" element={<SearchAll />} />
         <Route path="/upload" element={<UploadPage />} />
         <Route path="/post" element={<NewPost />} />
       </Routes>
