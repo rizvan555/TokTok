c21273f9c59b6a97909328c61a0f3fc0ef30c4a7
Jenseko <jenscode23@gmail.com>
2023-06-27 15:17:13 +0200

    start with profile styling : add style variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
+import OwnProfile from "./pages/OwnProfile";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<OwnProfile />} />
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+// Import Font
+import './resource/fonts/Urbanist-VariableFont_wght.ttf';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
