c421dca54b0eaacf9dad40142d8e0543b24fdf51
RizvanKarimov <karimovrizvan84@gmail.com>
2023-06-29 15:48:04 +0200

    created scroll setting page

--- src/components/ProfileFacts.js
+++ src/components/ProfileFacts.js
@@ -1,41 +1,55 @@
-
 // Import - Images -----------------------------------
 
-import profile_image from '../resource/images/Ellipseprofile_image_small.png';
-import profile_edit_icon from '../resource/icons/Edit Squareprofile_image_edit_icon.png';
+import profile_image from "../resource/images/Ellipseprofile_image_small.png";
+import profile_edit_icon from "../resource/icons/Edit Squareprofile_image_edit_icon.png";
 
 // ---------------------------------------------------
 
-const ProfileFacts = () => {
-    return (
-        <div>
-            <article className='profile_article'>
-                <div className='image-container'>
-                    <img src={profile_image} className='profile_img' alt="profile_image_user" />
-                    <img src={profile_edit_icon} className='edit_icon' alt="edit_icon" />
-                </div>
-                <h1>John Doe</h1>
-                <h3>UI/UX Designer</h3>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio magni totam, harum exercitationem accusamus facere praesentium expedita.</p>
-                <p>www.yourdomain.com</p>
-            </article>
-            <section className='follower_section'>
-                <div className='posts'>
-                    <h5>356</h5>
-                    <p>Posts</p>
-                </div>
-                <div className='followers'>
-                    <h5>46,379</h5>
-                    <p>Followers</p>
-                </div>
-                <div className='following'>
-                    <h5>318</h5>
-                    <p>Following</p>
-                </div>
-            </section>
-            <div className='follower_section_line'></div>
+const ProfileFacts = ({ click, setClick }) => {
+  return (
+    <div>
+      <article className="profile_article">
+        <div className="image-container">
+          <img
+            src={profile_image}
+            className="profile_img"
+            alt="profile_image_user"
+          />
+          <img src={profile_edit_icon} className="edit_icon" alt="edit_icon" />
         </div>
-    );
-}
+        <h1>John Doe</h1>
+        {!click ? <h3>UI/UX Designer</h3> : ""}
+        {!click ? (
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio magni
+            totam, harum exercitationem accusamus facere praesentium expedita.
+          </p>
+        ) : (
+          ""
+        )}
+        {!click ? <p>www.toktok.com</p> : ""}
+      </article>
+      {!click ? (
+        <section className="follower_section">
+          <div className="posts">
+            <h5>356</h5>
+            <p>Posts</p>
+          </div>
+          <div className="followers">
+            <h5>46,379</h5>
+            <p>Followers</p>
+          </div>
+          <div className="following">
+            <h5>318</h5>
+            <p>Following</p>
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
+      <div className="follower_section_line"></div>
+    </div>
+  );
+};
 
 export default ProfileFacts;
--- src/components/ProfileMainBar.js
+++ src/components/ProfileMainBar.js
@@ -1,27 +1,30 @@
-
 // Import - Images -----------------------------------
 
-import logo from '../resource/logos/toktokLogo.png';
-import postIcon from '../resource/icons/Plusframe_rounded.png';
-import editIcon from '../resource/icons/EditeditIcon.png';
-import settingIcon from '../resource/icons/GroupsettingIcon.png';
+import logo from "../resource/logos/toktokLogo.png";
+import postIcon from "../resource/icons/Plusframe_rounded.png";
+import editIcon from "../resource/icons/EditeditIcon.png";
+import settingIcon from "../resource/icons/GroupsettingIcon.png";
+import { useState } from "react";
 
 // ---------------------------------------------------
 
-const ProfileMainBar = () => {
-    return (
-        <nav className='profile_mainbar'>
-            <section>
-                <img src={logo} alt="logo toktok klein" />
-                <h2>john_doe</h2>
-            </section>
-            <nav>
-                <img src={postIcon} alt='icon_post_small' />
-                <img src={editIcon} alt='icon_edit_small' />
-                <img src={settingIcon} alt="icon_settings_small" />
-            </nav>
-        </nav>
-    );
-}
+const ProfileMainBar = ({click,setClick}) => {
+
+  return (
+    <nav className="profile_mainbar">
+      <section>
+        <img src={logo} alt="logo toktok klein" />
+        <h2>john_doe</h2>
+      </section>
+      <nav>
+        <img src={postIcon} alt="icon_post_small" />
+        <img src={editIcon} alt="icon_edit_small" />
+        <button className="settingsButton" onClick={() => setClick(!click)}>
+          <img src={settingIcon} alt="icon_settings_small" />
+        </button>
+      </nav>
+    </nav>
+  );
+};
 
 export default ProfileMainBar;
