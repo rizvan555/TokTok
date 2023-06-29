c72099f7e4b038553abbe29aeac15a50ea6df7a8
Jenseko <jenscode23@gmail.com>
2023-06-29 12:04:45 +0200

    input fields finished on editProfile Page

--- src/components/EditProfileTextInput.js
+++ src/components/EditProfileTextInput.js
@@ -1,17 +1,19 @@
 const EditProfileTextInput = () => {
     return (
         <div className="text_form">
-            <input type="text" name="name" id="name" placeholder="...name" />
-            <input type="text" name="username" id="username" placeholder="...username" />
-            <input type="text" name="activity" id="activity" placeholder="...activity" />
-            <input type="date" name="date" id="date" placeholder="...birth" />
-            <input type="email" name="email" id="email" placeholder="...email" />
-            <input type="number" name="phone" id="phone" placeholder="...phone" />
-            <select name="sex" id="sex"></select>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            {/* <option value="other">Other</option> */}
-            <input type="text" name="website" id="website" placeholder="...website" />
+            <input type="text" name="name" id="name" placeholder="name" />
+            <input type="text" name="username" id="username" placeholder="username" />
+            <input type="text" name="activity" id="activity" placeholder="activity" />
+            <input type="number" name="date" id="date" placeholder="birth" />
+            <input type="email" name="email" id="email" placeholder="email" />
+            <input type="number" name="phone" id="phone" placeholder="phone" />
+            <select name="sex" id="sex" placeholder="sex">
+                <option value=" " >sex</option>
+                <option value="male">male</option>
+                <option value="female">female</option>
+                <option value="other">other</option>
+            </select>
+            <input type="text" name="website" id="website" placeholder="website" />
         </div>
     );
 }
