b1a448a67dee79f085be8f20ee0217dd75a46693
Jenseko <jenscode23@gmail.com>
2023-06-29 11:04:13 +0200

    start add input fields on editProfile Page

--- src/components/EditProfileImage.js
+++ src/components/EditProfileImage.js
@@ -3,9 +3,9 @@ import profile_edit_icon from '../resource/icons/Edit Squareprofile_image_edit_i
 
 const EditProfileImage = () => {
     return (
-        <div className='UserImage'>
-            <img src={newUserImage} alt="new_user_image" />
-            <img src={profile_edit_icon} alt="profile_edit_icon" />
+        <div className='image-container'>
+            <img src={newUserImage} className='profile_image' alt="new_user_image" />
+            <img src={profile_edit_icon} className='edit_icon' alt="profile_edit_icon" />
         </div>
     );
 }
--- src/components/EditProfileTextInput.js
+++ src/components/EditProfileTextInput.js
@@ -1,7 +1,17 @@
 const EditProfileTextInput = () => {
     return (
-        <div>
-
+        <div className="text_form">
+            <input type="text" name="name" id="name" placeholder="...name" />
+            <input type="text" name="username" id="username" placeholder="...username" />
+            <input type="text" name="activity" id="activity" placeholder="...activity" />
+            <input type="date" name="date" id="date" placeholder="...birth" />
+            <input type="email" name="email" id="email" placeholder="...email" />
+            <input type="number" name="phone" id="phone" placeholder="...phone" />
+            <select name="sex" id="sex"></select>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            {/* <option value="other">Other</option> */}
+            <input type="text" name="website" id="website" placeholder="...website" />
         </div>
     );
 }
