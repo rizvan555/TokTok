93b8de8cfcf52b7921281a6548837780ca899fd0
codesfromannywhere <codesfromannywhere@gmail.com>
2023-06-30 09:12:55 +0200

    formatierung geändert

--- backend/model/CommentModel.js
+++ backend/model/CommentModel.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema({
     user: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
     content: { type: String },
+    post: { type: mongoose.SchemaTypes.ObjectId, ref: "Post" },
+    createdAt: new Date(),
 
 });
 
