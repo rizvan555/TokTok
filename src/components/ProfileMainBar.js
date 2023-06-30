dfea8af4b09326aadb39b735cf61d7fcfeefb292
Jenseko <jenscode23@gmail.com>
2023-06-30 09:14:27 +0200

    some bugfixes

--- src/components/ProfileMainBar.js
+++ src/components/ProfileMainBar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 // Import - Images -----------------------------------
 
 import logo from "../resource/logos/toktokLogo.png";
@@ -13,12 +15,12 @@ const ProfileMainBar = ({ click, setClick }) => {
     return (
         <nav className="profile_mainbar">
             <section>
-                <img src={logo} alt="logo toktok klein" />
+                <Link to='/'><img src={logo} alt="logo toktok klein" /></Link>
                 <h2>john_doe</h2>
             </section>
             <nav>
-                <img src={postIcon} alt="icon_post_small" />
-                <img src={editIcon} alt="icon_edit_small" />
+                <Link to='/upload'><img src={postIcon} alt="icon_post_small" /></Link>
+                <Link to='/editprofile'><img src={editIcon} alt="icon_edit_small" /></Link>
                 <button className="settingsButton" onClick={() => setClick(!click)}>
                     <img src={settingIcon} alt="icon_settings_small" />
                 </button>
