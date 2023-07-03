ef8fea2490f41beaad7d74351893b575832ea8c7
Jenseko <jenscode23@gmail.com>
2023-07-03 10:31:59 +0200

    fix2

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,5 +1,11 @@
-import { Link } from "react-router-dom";
+
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+
+
 import leftArrowImage from "../resource/icons/Groupleft_arrow_back.svg"
+
 
 // Import - components ----------------
 
@@ -14,20 +20,60 @@ import '../css/editProfile.css';
 // ------------------------------------
 
 
-const EditProfile = () => {
-    return (
-        <div className="editprofile_page">
-            <section className="editprofile_topbar">
-                <Link to='/profile'><img src={leftArrowImage} alt="left_arrow_icon" /></Link>
-                <h5>Edit Profile</h5>
-            </section>
-            <form>
-                <EditProfileImage />
-                <EditProfileTextInput />
-                <UpdateButton />
-            </form>
-        </div>
-    );
+const EditProfile = ({ darkLight, setDarkLight }) => {
+
+  const { state: navState } = useLocation();
+  const nav = useNavigate();
+  const [error, setError] = useState(navState?.redirectReason || "");
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    activity: "",
+    birthday: "",
+    email: "",
+    tel: "",
+    gender: "",
+    website: "",
+    aboutMe: "",
+  })
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+    console.log("handleSubmit");
+    try {
+      await axios.put("/api/user", user, { withCredentials: true })
+      nav("/profile");
+
+    } catch (error) {
+      const responseError = error?.response?.data?.error?.message;
+      if (responseError) {
+        setError(responseError);
+      } else {
+        setError("Something went wrong. Please try again later.")
+      }
+    }
+  }
+
+
+  return (
+    <div className="editprofile_page">
+      <section className="editprofile_topbar">
+        <Link to="/profile">
+          <BsArrowLeft
+            size={25}
+            style={{ color: !darkLight ? "white" : "black" }}
+          />
+        </Link>
+        <h5 style={{ color: !darkLight ? "white" : "black" }}>Edit Profile</h5>
+      </section>
+      <form onSubmit={handleSubmit} >
+        <EditProfileImage />
+        <EditProfileTextInput user={user} setUser={setUser} />
+        <UpdateButton />
+      </form>
+    </div>
+  );
 }
 
 export default EditProfile;
