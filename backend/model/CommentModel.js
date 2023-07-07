c4803cda57cc69e3eff1bd73ce06ecc9568c88e5
RizvanKarimov <karimovrizvan84@gmail.com>
2023-07-07 13:56:38 +0200

    commentsPage ready about darklight mode

--- backend/model/CommentModel.js
+++ backend/model/CommentModel.js
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-
 const commentSchema = new mongoose.Schema({
-    user: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
-    content: { type: String },
-    post: { type: mongoose.SchemaTypes.ObjectId, ref: "Post" },
-    likeCount: { type: Number },
-    // createdAt: new Date(),
-    feedbacks: { type: mongoose.SchemaTypes.ObjectId, ref: "Feedback" },
-
+  user: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
+  content: { type: String },
+  post: { type: mongoose.SchemaTypes.ObjectId, ref: "Post" },
+  likeCount: { type: Number, default: 0 },
+  // createdAt: new Date(),
+  feedback: { type: mongoose.SchemaTypes.ObjectId, ref: "Feedback" },
 });
 
 const feedbackSchema = new mongoose.Schema({
-    user: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
-    content: { type: String },
-})
+  user: { type: mongoose.SchemaTypes.ObjectId, ref: "User" },
+  content: { type: String },
+});
 
 export const feedback = mongoose.model("Feeback", feedbackSchema);
 
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -5,79 +5,89 @@ import { Link, useNavigate, useLocation } from "react-router-dom";
 // Import - Style -----------------------
 
 import "../css/signin.css";
-import toktokLogo_big from '../resource/logos/toktokLogo-big.png';
-import mailIcon from '../resource/icons/Messageemail_icon-filed.png';
-import showIcon from '../resource/icons/view-eye-svgrepo-com.svg';
-import hideIcon from '../resource/icons/eye-off-svgrepo-com.svg';
+import toktokLogo_big from "../resource/logos/toktokLogo-big.png";
+import mailIcon from "../resource/icons/Messageemail_icon-filed.png";
+import showIcon from "../resource/icons/view-eye-svgrepo-com.svg";
+import hideIcon from "../resource/icons/eye-off-svgrepo-com.svg";
 
 // --------------------------------------
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const { state: navState } = useLocation();
+  const [error, setError] = useState(navState?.redirectReason || "");
 
-    const navigate = useNavigate()
-    const { state: navState } = useLocation();
-    const [error, setError] = useState(navState?.redirectReason || "");
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [user, setUser] = useState({
-        email: "",
-        password: ""
-    })
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [showPassword, setShowPassword] = useState(false);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError("");
+    // Um dies zu prüfen, Inputfeler im Frontend ausfüllen
+    console.log(user);
+    await axios
+      .post("/api/signin", user)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((error) => {
+        const responseError = error?.response?.data?.error?.message;
+        if (responseError) {
+          setError(responseError);
+        } else {
+          setError("Something went wrong please try later");
+        }
+      });
+  };
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        setError("");
-        // Um dies zu prüfen, Inputfeler im Frontend ausfüllen
-        console.log(user);
-        await axios.post("/api/signin", user)
-            .then((res) => {
-                console.log(res)
-                navigate("/")
-            }).catch((error) => {
-                const responseError = error?.response?.data?.error?.message;
-                if (responseError) {
-                    setError(responseError);
-                } else {
-                    setError("Something went wrong please try later");
-                }
-            })
-    }
-
-    return (
-        <div className="signin_page">
-            <h2 className="headline">Login to your Account</h2>
-            <img className="toktokLogo" src={toktokLogo_big} alt="tiktokLogo_big" />
-            <form className="signin_form" onSubmit={handleLogin}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    className="email"
-                    value={user.email}
-                    onChange={(e) => {
-                        setUser({ ...user, email: e.target.value })
-                    }} />
-                <img className="mailIcon" src={mailIcon} alt="mail_icon" />
-                <input
-                    type={showPassword ? "text" : "password"}
-                    placeholder="Password"
-                    className="password"
-                    value={user.password}
-                    onChange={(e) => {
-                        setUser({ ...user, password: e.target.value })
-                    }} />
-                <small>{error}</small>
-                <span
-                    className="toggle_password"
-                    onClick={() => setShowPassword(!showPassword)}>
-                    {showPassword ? <img className="showIcon" src={showIcon} /> : <img className="hideIcon" src={hideIcon} />}
-                </span>
-                <button className="signin_btn">Log In</button>
-            </form>
-            <Link className="forgot_link">Forgot the password?</Link>
-            <p className="link_to">Don't have an account? <Link to={"/signup"}>Sign up</Link></p>
-        </div>
-    );
-}
+  return (
+    <div className="signin_page">
+      <h2 className="headline">Login to your Account</h2>
+      <img className="toktokLogo" src={toktokLogo_big} alt="tiktokLogo_big" />
+      <form className="signin_form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          className="email"
+          value={user.email}
+          onChange={(e) => {
+            setUser({ ...user, email: e.target.value });
+          }}
+        />
+        <img className="mailIcon" src={mailIcon} alt="mail_icon" />
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          className="password"
+          value={user.password}
+          onChange={(e) => {
+            setUser({ ...user, password: e.target.value });
+          }}
+        />
+        <small>{error}</small>
+        <span
+          className="toggle_password"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? (
+            <img className="showIcon" src={showIcon} />
+          ) : (
+            <img className="hideIcon" src={hideIcon} />
+          )}
+        </span>
+        <button className="signin_btn">Log In</button>
+      </form>
+      <Link className="forgot_link">Forgot the password?</Link>
+      <p className="link_to">
+        Don't have an account? <Link to={"/signup"}>Sign up</Link>
+      </p>
+    </div>
+  );
+};
 
 export default SignIn;
